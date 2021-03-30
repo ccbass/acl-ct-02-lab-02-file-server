6e3f90bbfde5e70fd9ea86c9bd0547a3127b007f
Colin Bassett <ccwbassett@gmail.com>
2021-03-29 19:54:11 -0700

    passing, access through res.text

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -2,5 +2,28 @@ const request = require('supertest');
 const app = require('../lib/app');
 
 describe('app routes', () => {
+    it('Returns "hi" on GET /', async() => {
+        const res = await request(app)
+            .get('/');
+
+        expect(res.text).toEqual('hi');
+    });
+
+    it('Echoes req.body and returns 200 on POST /', async() => {
+        const body = { 'someData': 'this is in a body!' };
+
+        const res = await request(app)
+            .post('/')
+            .send(body);
+
+        expect(res.text).toEqual(JSON.stringify(body));
+    });
+
+    it('Returns the color on GET /color', async() => {
+        const res = await request(app)
+            .get('/red');
+
+        expect(res.text).toEqual('<!DOCTYPE html><html><body><h1>red</h1></body></html>');
+    });
 
 });
