54f100b1466a1900243cbc843decebf79fa70233
Colin Bassett <ccwbassett@gmail.com>
2021-03-31 11:46:00 -0700

    passing app routes tests.

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -2,7 +2,39 @@ const request = require('supertest');
 const app = require('../lib/app');
 
 describe('app routes', () => {
-    it.skip('tests GET', () => {
+    it('Returns the file on a valid GET URL', async() => {
+        const res = await request(app)
+            .get('/index.html');
 
+        expect(res.text).toEqual(
+    `<!DOCTYPE html>\r
+<html lang="en">\r
+<head>\r
+    <meta charset="UTF-8">\r
+    <title>Index</title>\r
+</head>\r
+<body>\r
+    <h1>This is an HTML document!</h1>\r
+</body>\r
+</html>`
+        );
+        expect(res.statusCode).toEqual(200);
     });
+
+    it('Returns 404 on valid URL, missing file', async() => {
+        const res = await request(app)
+            .get('/missingfile.html');
+
+        expect(res.text).toEqual('');
+        expect(res.statusCode).toEqual(404);
+    });
+
+    it('Returns 404 on invalid URL', async() => {
+        const res = await request(app)
+            .get('/secrets/index.html');
+
+        expect(res.text).toEqual('');
+        expect(res.statusCode).toEqual(404);
+    });
+
 });
